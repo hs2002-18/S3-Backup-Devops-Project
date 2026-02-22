bed162eedc7b143a3b8ff9337841acbf4737db2a
Harsh Shrimali <harshshrimali319@gmail.com>
2026-02-22 03:38:20 +0000

    updated file path

--- s3_backup.py
+++ s3_backup.py
@@ -7,7 +7,7 @@ def upload_backup(s3, file_name, bucket_name, key_name):
     s3.Bucket(bucket_name).put_object(Key=key_name, Body = data)
     print("Uploaded!")
 
-file_name = "/home/harshshrimali319/s3-backup-devops/file_to_be_backedup.txt"
+file_name = "file_to_be_backedup.txt"
 bucket_name = "automation-s3-project-bucket"
 key_name = "backup " +str(uuid.uuid4()) +".tar.gz"
 
